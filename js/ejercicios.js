4d5d54ba060ebbaf1e4a01bea7e903d7ddd508b2
IARA SZWARSTEIN <49193480@ort>
2025-09-05 08:52:14 -0300

    hasta el 7 y correcion

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,11 +1,11 @@
 //ejercicio 1
-function calcularEdad(){ 
+function calcularEdadJs(){ 
     console.log("ejercicio1")
-   const nacimientoa=fetch(document.getElementById("nacimiento"))
 const nacimiento=document.getElementById("nacimiento");
 const nombre=document.getElementById("nombre");
-const edad=calcularEdad(nacimiento)
-console.log(`Hola ${nombre}, tienes ${edad} años!`)
+if(nombre.value.length==0 || nacimiento.value.length==0){console.log("Ingresa toda la informacion antes de calcular");return;}
+const edad=calcularEdad(nacimiento.value)
+console.log(`Hola ${nombre.value}, tienes ${edad} años!`)
 }
 
 //ejericio 2
@@ -36,16 +36,52 @@ function comparardiez(){
 }
 
 //ejercicio 4
-function ciudades(){
+function mostrarCiudad(){
    const ciudad = {
     nombre:'Buenos Aires',
     fechaFundacion:'1990-02-02',
     poblacion:283874739,
-    extension:'a'
+    extension:'3939029KM'
+   }
+   for (x in ciudad){
+    console.log("Clave: "+x+" Valor: "+ciudad[x]);
    }
 }
 
 //ejercicio 5
-function doblenum(numeros){
-   numeros
+function dobleNum(numeros){
+   const nueva=numeros.map(num => num*2);
+   for(num of nueva){
+    console.log(num);
+   }
+}
+
+//ejercicio 6
+function asteriscos(){
+    console.log("A:")
+    for(let i=1;i<6;i++){
+        console.log("*".repeat(i))
+    }
+    console.log("B:")
+    let j=4;
+    for(let k=1;k<11;k=k+2){
+        if(j<0){
+            console.log("*".repeat(k));
+        }else{
+            console.log("-".repeat(j)+"*".repeat(k)+"-".repeat(j));
+            j=j-1;
+        }
+
+    }
+}
+
+//ejercicio7
+function nombresA(){
+    let nombres=document.getElementById("listaNombres").value;
+    nombres=nombres.split(" ");
+    let nombresA=nombres.filter(nombre => nombre.startsWith('A'));
+    for(nombre in nombresA){
+        console.log(nombre)
+    }
+
 }
--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,18 @@
 function calcularEdad(nacimiento){
-    console.log(nacimiento.slice(0,3)) 
-    let anionacimiento=parseInt(nacimiento.slice(0,3));
-    let ahora=Date.now();
+    console.log(nacimiento.slice(0,4)) 
+    const anionacimiento=parseInt(nacimiento.slice(0,4));
+    const mesNacimiento=parseInt(nacimiento.slice(5,7));
+    const diaNacimiento=parseInt(nacimiento.slice(7,10))
+    let ahora=new Date();
+    const mes=ahora.getMonth()+1;
+    const dia=ahora.getDay();
     ahora=ahora.getFullYear();
-    return ahora-anionacimiento;
+    let edad=ahora-anionacimiento;
+    if(mes<=mesNacimiento){
+        if(mes==mesNacimiento){
+            if(dia<diaNacimiento){
+                edad=edad-1;}}else{
+                    edad=edad-1;
+                }}
+    return edad;
 }
